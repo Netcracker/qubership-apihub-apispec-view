e4a0478d88baed4feb2e46b7bba32351b97d8f58
Iurii Golovinskii <iurii-golovinskii.qubership@gmail.com>
2025-02-25 17:40:32 +0300

    review 3 updated maps

--- packages/diff-block/src/constants.ts
+++ packages/diff-block/src/constants.ts
@@ -1,4 +1,12 @@
-import { ClassifierType, DiffType } from '@netcracker/qubership-apihub-api-diff';
+import {
+  annotation,
+  breaking,
+  ClassifierType,
+  deprecated,
+  DiffType,
+  nonBreaking,
+  semiBreaking, unclassified,
+} from '@netcracker/qubership-apihub-api-diff'
 import { keys } from 'lodash';
 
 export const diffMetaKey = Symbol('diffMeta');
@@ -9,38 +17,31 @@ export const syntheticTitleFlag = Symbol('syntheticTitleFlag');
 
 export const DIFF_TYPES: DiffType[] = keys(ClassifierType) as DiffType[];
 
-export const BREAKING_CHANGE_SEVERITY = 'breaking'
-export const NON_BREAKING_CHANGE_SEVERITY = 'non-breaking'
-export const SEMI_BREAKING_CHANGE_SEVERITY = 'semi-breaking'
-export const DEPRECATED_CHANGE_SEVERITY = 'deprecated'
-export const ANNOTATION_CHANGE_SEVERITY = 'annotation'
-export const UNCLASSIFIED_CHANGE_SEVERITY = 'unclassified'
-
 export const DIFF_TYPE_COLOR_MAP = {
-  breaking: '#ED4A54',
-  deprecated: '#F4B24D',
-  'non-breaking': '#6BCE70',
-  'semi-breaking': '#E98554',
-  annotation: '#C55DCF',
-  unclassified: '#61AAF2',
+  [breaking]: '#ED4A54',
+  [deprecated]: '#F4B24D',
+  [nonBreaking]: '#6BCE70',
+  [semiBreaking]: '#E98554',
+  [annotation]: '#C55DCF',
+  [unclassified]: '#61AAF2',
 };
 
 export const DIFF_BUDGES_COLOR_MAP = {
-  breaking: '#DC5759',
-  deprecated: '#F4B24D',
-  'non-breaking': '#6BCE70',
-  'semi-breaking': '#E98554',
-  annotation: '#B866C9',
-  unclassified: '#70A9EC',
+  [breaking]: '#DC5759',
+  [deprecated]: '#F4B24D',
+  [nonBreaking]: '#6BCE70',
+  [semiBreaking]: '#E98554',
+  [annotation]: '#B866C9',
+  [unclassified]: '#70A9EC',
 };
 
 export const CHANGE_SEVERITY_NAME_MAP: Record<DiffType, string> = {
-  [BREAKING_CHANGE_SEVERITY]: 'breaking',
-  [SEMI_BREAKING_CHANGE_SEVERITY]: 'risky',
-  [DEPRECATED_CHANGE_SEVERITY]: 'deprecated',
-  [NON_BREAKING_CHANGE_SEVERITY]: 'non-breaking',
-  [UNCLASSIFIED_CHANGE_SEVERITY]: 'unclassified',
-  [ANNOTATION_CHANGE_SEVERITY]: 'annotation',
+  [breaking]: 'breaking',
+  [semiBreaking]: 'risky',
+  [deprecated]: 'deprecated',
+  [nonBreaking]: 'non-breaking',
+  [unclassified]: 'unclassified',
+  [annotation]: 'annotation',
 }
 export type WithDiffMetaKey<T> = T & {
   [diffMetaKey]: any;
