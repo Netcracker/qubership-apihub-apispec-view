56c5c83fdb061a12802521e7aa3905d886556bc8
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-06-22 15:06:45 +0400

    fix: fix indents one more time

--- packages/diff-block/src/DiffContainer.tsx
+++ packages/diff-block/src/DiffContainer.tsx
@@ -6,6 +6,8 @@ import { DiffContextProvider, useDiffContext } from './DiffContext';
 
 type DiffContainerProps = { children: React.ReactElement };
 
+const PADDING_X = 16;
+
 export const DiffContainer: React.FC<DiffContainerProps> = ({ children }) => {
   const [ref, setRef] = React.useState<HTMLDivElement | null>(null);
 
@@ -33,14 +35,14 @@ export const DiffContainer: React.FC<DiffContainerProps> = ({ children }) => {
           overflowY="hidden"
         />
       )}
-      <div style={{ flex: `0 1 ${isCompareMode ? '50%' : '100%'}`, minWidth: 0, zIndex: 10, paddingLeft: 8, paddingRight: 8 }}>
+      <div style={{ flex: `0 1 ${isCompareMode ? '50%' : '100%'}`, minWidth: 0, zIndex: 10, paddingLeft: PADDING_X, paddingRight: PADDING_X }}>
         <DiffContextProvider containerElement={ref} side={isCompareMode ? 'before' : 'undefined'}>
           {children}
         </DiffContextProvider>
       </div>
       {isCompareMode && (
         <>
-          <div style={{ flex: '0 1 50%', minWidth: 0, zIndex: 10, paddingLeft: 8, paddingRight: 8 }}>
+          <div style={{ flex: '0 1 50%', minWidth: 0, zIndex: 10, paddingLeft: PADDING_X, paddingRight: PADDING_X }}>
             <DiffContextProvider containerElement={ref} side="after">
               {React.Children.only(React.cloneElement(children))}
             </DiffContextProvider>
